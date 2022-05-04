737e0611de97bc6d1ace6054c34939a26dbefda5
anshumansingh005 <anshumansingh005@gmail.com>
2022-05-04 21:44:53 +0530

    Added CSS file

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,4 +1,13 @@
+import "./ExpenseItem.css";
 function ExpenseItem() {
-  return <h2>This is Expense Item</h2>;
+  return (
+    <div className="expense-item">
+      <div>March 28th 2021</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$290</div>
+      </div>
+    </div>
+  );
 }
 export default ExpenseItem;
